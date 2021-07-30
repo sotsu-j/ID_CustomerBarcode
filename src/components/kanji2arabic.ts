20c7ddc5861072a6e430c7df44ccd8f675f9366e
SJ <sotsu@adachi-printing.co.jp>
2021-07-30 11:58:08 +0900

    分離

---
+++ src/components/kanji2arabic.ts
@@ -0,0 +1,53 @@
+
+const units = ["丁目", "丁 ", "番地", "番", "号", "地割", "線", "の", "ノ"];
+const k_numbers = "〇一二三四五六七八九零壱弐参肆伍陸質捌玖零壹貳參";
+const k_digit_1 = "十百千拾佰仟十陌阡";
+const k_digit_2 = "万億兆萬";
+
+export const kanji2arabic = (k_number: string) => {
+    const numbers = k_number.split('').reverse();
+    const result = numbers.reduce(({ sum, magnification, ballast, digit }, str) => {
+        const num = k_numbers.split('').indexOf(str) % 10;
+        const digit1Index = k_digit_1.split('').indexOf(str);
+        const digit2Index = k_digit_2.split('').indexOf(str);
+        if (digit2Index > -1) {
+            magnification = [10000, 100000000, 1000000000000][digit2Index % 3];
+            digit = magnification;
+            sum += digit;
+            ballast = true;
+        } else if (digit1Index > -1) {
+            digit = [10, 100, 1000][digit1Index % 3] * magnification;
+            if (ballast && magnification > 1000) {
+                sum -= magnification;
+            }
+            sum += digit;
+            ballast = true;
+        } else {
+            sum += (num > 0 ? (num + (ballast ? -1 : 0)) * digit : 0);
+            digit *= 10;
+            ballast = false;
+        }
+        return { sum, digit, ballast, magnification };
+    }, { sum: 0, digit: 1, ballast: false, magnification: 1 });
+    return String(result.sum);
+}
+
+export const kanji2arabicOnAddress = (address: string) => {
+    const re = new RegExp(`([${k_numbers}${k_digit_1}${k_digit_2}]+(${units.join('|')}))`, 'g');
+    const targets = address.match(re);
+    if (targets) {
+        const result = targets.reduce((accumulator, target) => {
+            const re = new RegExp(`([${k_numbers}${k_digit_1}${k_digit_2}]+)(${units.join('|')})`);
+            const match = target.match(re);
+            if (match && match.length >= 3) {
+                const k_number = match[1];
+                const word = match[2];
+                const arabic = kanji2arabic(k_number);
+                return accumulator.replace(`${k_number}${word}`, `${arabic}${word}`);
+            }
+            return accumulator;
+        }, address);
+        return result;
+    }
+    return address;
+}
--- src/components/transformAddressToCodeElement.ts
+++ src/components/transformAddressToCodeElement.ts
@@ -4,6 +4,7 @@
 /// https://www.post.japanpost.jp/zipcode/zipmanual/p17.html
 ///
 ////////////////////////////////////////////////////////////////
+import { kanji2arabicOnAddress } from "./kanji2arabic";
 
 export const transformAddressToCodeElement = (address: string) => {
     const supplementaryRules_1 = kanji2arabicOnAddress(address);
@@ -14,57 +15,4 @@ export const transformAddressToCodeElement = (address: string) => {
     const phase_4 = phase_3.replace(/[^0-9A-Z\-]/, '-');
     const phase_5 = phase_4.replace(/\-{2,}/, '-');
     const phase_6 = phase_5.replace(/^\-/, '');
-}
-
-const units = ["丁目", "丁 ", "番地", "番", "号", "地割", "線", "の", "ノ"];
-const k_numbers = "〇一二三四五六七八九零壱弐参肆伍陸質捌玖零壹貳參";
-const k_digit_1 = "十百千拾佰仟十陌阡";
-const k_digit_2 = "万億兆萬";
-
-const kanji2arabic = (k_number: string) => {
-    const numbers = k_number.split('').reverse();
-    const result = numbers.reduce(({ sum, magnification, ballast, digit }, str) => {
-        const num = k_numbers.split('').indexOf(str) % 10;
-        const digit1Index = k_digit_1.split('').indexOf(str);
-        const digit2Index = k_digit_2.split('').indexOf(str);
-        if (digit2Index > -1) {
-            magnification = [10000, 100000000, 1000000000000][digit2Index % 3];
-            digit = magnification;
-            sum += digit;
-            ballast = true;
-        } else if (digit1Index > -1) {
-            digit = [10, 100, 1000][digit1Index % 3] * magnification;
-            if (ballast && magnification > 1000) {
-                sum -= magnification;
-            }
-            sum += digit;
-            ballast = true;
-        } else {
-            sum += (num > 0 ? (num + (ballast ? -1 : 0)) * digit : 0);
-            digit *= 10;
-            ballast = false;
-        }
-        return { sum, digit, ballast, magnification };
-    }, { sum: 0, digit: 1, ballast: false, magnification: 1 });
-    return String(result.sum);
-}
-
-const kanji2arabicOnAddress = (address: string) => {
-    const re = new RegExp(`([${k_numbers}${k_digit_1}${k_digit_2}]+(${units.join('|')}))`, 'g');
-    const targets = address.match(re);
-    if (targets) {
-        const result = targets.reduce((accumulator, target) => {
-            const re = new RegExp(`([${k_numbers}${k_digit_1}${k_digit_2}]+)(${units.join('|')})`);
-            const match = target.match(re);
-            if (match && match.length >= 3) {
-                const k_number = match[1];
-                const word = match[2];
-                const arabic = kanji2arabic(k_number);
-                return accumulator.replace(`${k_number}${word}`, `${arabic}${word}`);
-            }
-            return accumulator;
-        }, address);
-        return result;
-    }
-    return address;
 }
