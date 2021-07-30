6b623ebaa674f2efc8968d98bd2db6869f40c404
SJ <sotsu@adachi-printing.co.jp>
2021-07-30 13:25:42 +0900

    住所２からコード生成

--- src/components/transformAddressToCodeElement.ts
+++ src/components/transformAddressToCodeElement.ts
@@ -11,8 +11,15 @@ export const transformAddressToCodeElement = (address: string) => {
 
     const phase_1 = supplementaryRules_1.toUpperCase();
     const phase_2 = phase_1.replace(/[&\/・\.]/g, '');
-    const phase_3 = phase_2.replace(/[A-Z]{2,}/, '');
-    const phase_4 = phase_3.replace(/[^0-9A-Z\-]/, '-');
-    const phase_5 = phase_4.replace(/\-{2,}/, '-');
+    const phase_3 = phase_2.replace(/[A-Z]{2,}/g, '');
+
+    const supplementaryRules_2_3 = phase_3.replace(/([0-9])F([^F]+?)/g, "$1-$2").replace(/([0-9])F/g, "$1");
+
+    const phase_4 = supplementaryRules_2_3.replace(/[^0-9A-Z\-]/g, '-');
+    const phase_5 = phase_4.replace(/\-{2,}/g, '-');
     const phase_6 = phase_5.replace(/^\-/, '');
+
+    const supplementaryRules_4_5 = phase_6.replace(/\-?([A-Z])\-?/g, "$1");
+
+    return supplementaryRules_4_5;
 }
